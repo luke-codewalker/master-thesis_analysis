80aec0115673c6bb148679137741d1eaf8edc469
Luke Codewalker <arathorn089@arcor.de>
2018-08-15 13:34:30 +0200

    renamed data cleaning script

--- clean_data.py
+++ clean_data.py
@@ -1,4 +1,5 @@
 import re
+from csv import writer
 
 for i in range(31):
     # convert range to vp number
@@ -6,8 +7,18 @@ for i in range(31):
     # open raw data file
     with open('experiment_data/raw_data/experiment-data_VP'+vp_number+'.csv', 'r') as csv_file:
         content = csv_file.read()
+
         # replace new lines after condition code (e.g. \d[a-z] = "5a")
         cleaned_content = re.sub(r'(\d[a-z])\n', r'\1', content)
+
+        # split the new cleaned file into lines
+        lines = cleaned_content.split('\n')
+        # 64 lines of data + header + trailing newline
+        regular_line_count = 66
+        if(len(lines) > regular_line_count):
+            # reduce to data lines + header
+            lines = lines[0:regular_line_count - 1]
+
         # write contents to new file
         with open('experiment_data/experiment-data_VP'+vp_number+'.csv', 'w') as new_file:
             new_file.write(cleaned_content)
