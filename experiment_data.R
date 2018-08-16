94d174351ae58230f82ed4a5d9f39a2afea9da72
Luke Codewalker <arathorn089@arcor.de>
2018-08-16 12:00:34 +0200

    added merge in one df and first crosstabs

--- experiment_data.R
+++ experiment_data.R
@@ -1,5 +1,6 @@
-# set working directory
+# set working directory and clean environment
 setwd("C:/Users/Lukas/Dropbox/Studium/TU Berlin/Masterarbeit/Analysis/")
+rm(list = ls())
 
 # read in data sets
 datafiles = list.files(path="./experiment_data", pattern=".csv", full.names = TRUE)
@@ -7,4 +8,35 @@ datasets = lapply(datafiles, function(x) read.csv(x, sep=";"))
 names(datasets) = gsub("./experiment_data/experiment-data_(VP\\d+).csv","\\1",datafiles)
 
 # check number formats and convert to int in miliseconds if neccessary
-datasets = lapply(datasets, function(x) lapply(x, function(y) if(typeof(y)=="double") y*1000 else y))
+datasets = lapply(datasets, function(x) {
+  lapply(x, function(y){
+    if(typeof(y)=="double") y*1000 else y
+  })  
+  return(data.frame(x))
+})
+
+# decode condition into variables
+datasets = lapply(datasets, function(x){ 
+  # stopCondition: 0 = no stop, 1 = stop
+  x$stopCondition <- ifelse(as.numeric(substr(as.character(x$condition),1,1))<4,0,1)
+  # delayCondition: 0 = no delay (signal on braking), 1 = signal before braking
+  x$delayCondition <- ifelse(substr(as.character(x$condition),2,2)=="a",0,1)
+  # signalCondition: 0 = none, 1 = eyes, 2 = ampel, 3 = both
+  x$signalCondition <- as.numeric(substr(as.character(x$condition),1,1))%%4
+  return(x)
+  })
+
+# merge cleaned list of datasets into one dataframe
+dataframe <- do.call("rbind", datasets)
+
+# table of decision for all participants
+xtabs(~reactionHand, dataframe)/nrow(dataframe) 
+
+# cross table of decision over stop condition for all participants
+xtabs(~reactionHand+stopCondition, dataframe)/nrow(dataframe)                  
+
+# cross table of decision over stop condition and stop condition for all participants
+xtabs(~reactionHand+signalCondition+stopCondition, dataframe)/nrow(dataframe)*2                  
+
+# cross table of decision over delay condition for all participants
+xtabs(~reactionHand+delayCondition, dataframe)/nrow(dataframe)                  
